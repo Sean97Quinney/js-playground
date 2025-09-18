0c684a02023905aea9d38b6d521a2de564defe21
Sean97Quinney <98776538+Sean97Quinney@users.noreply.github.com>
2025-09-18 19:28:28 +0100

    added variables and function examples

---
+++ basics/functions.js
@@ -0,0 +1,12 @@
+// Functions in JavaScript
+
+// Function declaration
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+console.log(greet("Sean"));
+
+// Arrow function (modern style)
+const square = (x) => x * x;
+console.log("Square of 5 is:", square(5));
---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+// Variables in JavaScript
+
+// "let" = changeable variable
+let name = "Sean";
+console.log("My name is:", name);
+
+// "const" = fixed variable (cannot be reassigned)
+const pi = 3.14159;
+console.log("The value of pi is:", pi);
+
+// Reassigning a let
+name = "Pubspotter";
+console.log("Now my name is:", name);
+
+// Old style (var) - not recommended
+var legacy = "I am var";
+console.log(legacy);
